7edb7995af65bfc1edd9a77ea0343b5d2cd2a988
Gabriel Corvalan <gabrielcorvalan@ip-192-168-1-42.us-west-2.compute.internal>
2022-06-09 19:12:17 -0300

    new project

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { SubtreeLibrarySample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  SubtreeLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <subtree-library-sample :initCounter="30" />
+  </div>
+</template>
